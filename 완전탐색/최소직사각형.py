4c3e39f599739273be00e360ca65809f8972fd82
hyojine <touroz@naver.com>
2023-01-07 00:10:16 +0900

    23/01/06

---
+++ 완전탐색/최소직사각형.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    w=[]
+    h=[]
+    for size in sizes:
+        if size[0]>size[1]:
+            w.append(size[1])
+            h.append(size[0])
+        else:
+            w.append(size[0])
+            h.append(size[1])
+    return max(w)*max(h)
