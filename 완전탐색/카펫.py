b63bd535f0c77225b44c6c2ceef3f755ea8dfd96
hyojine <touroz@naver.com>
2023-01-22 01:26:07 +0900

    완전탐색 4번 완료(2)

--- 완전탐색/카펫.py
+++ 완전탐색/카펫.py
@@ -1,15 +1,23 @@
-def solution(brown, yellow):
-    size = brown+yellow
-    w=0
-    h=0
+# def solution(brown, yellow):
+#     size = brown+yellow
+#     w=0
+#     h=0
 
-    while size!=w*h:
-        h+=1
-        w=int((brown+4)/2-h)
-        print(w,h)
-        print(type(w),type(h))
+#     while size!=w*h:
+#         h+=1
+#         w=int((brown+4)/2-h)
+#         print(w,h)
+#         print(type(w),type(h))
       
-    return [w,h]
+#     return [w,h]
+
+# yellow의 임의의 한변의 길이를 기준으로 품
+def solution(brown, yellow):
+    for i in range(1,yellow+1):
+        if yellow%i==0 and 2*(i+2+yellow/i)==brown:
+            print(yellow/i+2)
+            print(type(yellow/i+2))
+            return [int(yellow/i+2),i+2]
          
 print(solution(10,2))
 
