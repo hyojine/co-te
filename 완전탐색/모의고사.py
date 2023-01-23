cf030672d6287ce01da9bbfdf5ac523eadbc2c81
hyojine <touroz@naver.com>
2023-01-23 23:33:35 +0900

    완전탐색 2번 완료

---
+++ 완전탐색/모의고사.py
@@ -0,0 +1,47 @@
+# thislist=[2,3,4,3,3,5,6,6]
+# print(thislist.index(max(thislist)))
+# print(sorted(thislist))
+# a=thislist.sort() #[2, 3, 3, 3, 4, 5, 6, 6]
+# print(a) #None
+
+def solution(answers):
+    student = [[1, 2, 3, 4, 5],
+               [2, 1, 2, 3, 2, 4, 2, 5],
+               [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    answer_cnt={}
+    for i in range(len(student)):
+        count=0
+        while len(student[i])<len(answers):
+            student[i].extend(student[i])
+        if len(student[i])-len(answers):
+            student[i]=student[i][:-(len(student[i])-len(answers))]
+        for num in range(len(student[i])):
+            if student[i][num]==answers[num]:
+                count+=1
+        answer_cnt[i+1]=count
+    ans=[k for k,v in answer_cnt.items() if max(answer_cnt.values())==v]
+    print(ans)
+
+# 다르게
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
+
+solution([1,3,2,2,5,2,2,2,2,2,2,2])
