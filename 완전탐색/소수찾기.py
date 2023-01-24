1c50239df3b07a114c203d110aff74fc889dcc5f
hyojine <touroz@naver.com>
2023-01-24 21:48:56 +0900

    완전탐색 3번 완료(2)

--- 완전탐색/소수찾기.py
+++ 완전탐색/소수찾기.py
@@ -10,17 +10,34 @@
 #             p_cnt+=1
 #     return p_cnt
 
+# 통과
+# def solution(n):
+#     p_cnt=0
+#     for i in range(2,n+1):
+#         count=0 
+#         for j in range(1,int(i**(0.5))+1):
+#             if i%j==0:
+#                 count+=1
+#             if count>1:
+#                 break
+#         if count ==1:
+#             p_cnt+=1
+#     return p_cnt
+
+# 여기서
+# if i**0.5 == round(i,1):로 걸러주면 더 오래걸림
 def solution(n):
     p_cnt=0
     for i in range(2,n+1):
         count=0 
-        for j in range(1,int(i**(0.5))+1):
-            if i%j==0:
-                count+=1
-            if count>1:
-                break
-        if count ==1:
-            p_cnt+=1
-    return p_cnt
+        if i**0.5 != round(i**0.5,1):
+            for j in range(1,int(i**(0.5))+1):
+                if i%j==0:
+                    count+=1
+                if count>1:
+                    break
+            if count ==1:
+                p_cnt+=1
+    return p_cnt                
 
 print(solution(10))
