44766f7fdab8c21165f952275e813f0b27683531
hyojine <touroz@naver.com>
2023-01-25 14:11:57 +0900

    3주차 5번 완료(2)

--- 3주차/5.수박.py
+++ 3주차/5.수박.py
@@ -7,3 +7,8 @@ def solution(n):
             s+='박'
     return s
 print(solution(3))
+
+def solution(n):
+    return "".join(["수박"[i%2] for i in range(n)]) 
+    # 2로 나누니까 어차피 나머지가 0또는 1
+    # -> '수' 또는 '박'을 리스트에 담아서 공백없이 문자열로 합쳐줌
