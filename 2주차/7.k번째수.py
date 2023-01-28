eb4b2666f4435acc32cb328894d79041c864d462
hyojine <touroz@naver.com>
2023-01-29 03:07:54 +0900

    2주차 7번 완료(2)

--- 2주차/7.k번째수.py
+++ 2주차/7.k번째수.py
@@ -1,8 +1,7 @@
 def solution(array, commands):
     ans=[]
     for i in range(len(commands)):
-        slice=sorted(array[commands[i][0]-1:commands[i][1]])
-        ans.append(slice[commands[i][2]-1])
+        ans.append(sorted(array[commands[i][0]-1:commands[i][1]])[commands[i][2]-1])
     return ans
 
 array=[1, 5, 2, 6, 3, 7, 4]
