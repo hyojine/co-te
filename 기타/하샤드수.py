e3d38cc1ba20b2e6b8ada30a42d441b14e2bbbbf
hyojine <touroz@naver.com>
2023-01-31 00:59:46 +0900

    기타 완료(2)

--- 기타/하샤드수.py
+++ 기타/하샤드수.py
@@ -9,3 +9,25 @@ def solution(x):
         return True
     else:
         return False
+
+# 수정
+def solution(x):
+    total=0
+    y=x
+    while y>10:
+        total+=y%10
+        y=y//10
+    total+=y
+    return x%total==0
+
+# 스트링으로
+def solution(x):
+    x=str(x)
+    total=0
+    for i in x:
+        total+=int(i)
+    return int(x)%total==0
+    
+# 리스트 축약식으로
+def solution(x):
+    return x % sum([int(c) for c in str(x)]) == 0
