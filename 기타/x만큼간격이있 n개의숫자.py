141d7f53c1f5a8c6f2a77f8638da9ac43464033c
hyojine <touroz@naver.com>
2023-02-03 11:28:01 +0900

    기타 완료(3)

--- 기타/x만큼간격이있 n개의숫자.py
+++ 기타/x만큼간격이있 n개의숫자.py
@@ -8,3 +8,15 @@ def solution(x, n):
 
 def solution(x, n):
     return [x+(i)*x for i in range(n)]
+    # return [(i+1)*x for i in range(n)]
+    # i = 0~ n-1
+
+def solution(x, n):
+    return [x*i for i in range(1,n+1)]
+    # i = 1~ n
+
+def solution(x, n):
+    if x==0:
+        return [0]*n
+    return list(range(x, x*(n+1), x))
+    # range(start, end, gap)
