e3ec910b41a19eec147aa596f1e4f9b8dbb06544
hyojine <touroz@naver.com>
2023-02-04 22:30:16 +0900

    기타 완료(2)

--- 기타/문자열다루기기본.py
+++ 기타/문자열다루기기본.py
@@ -2,9 +2,6 @@ def solution(s):
     numbers='0123456789'
     return (len(s)==4 or len(s)==6) and (len([x for x in s if x in numbers])==len(s))
 
-print(solution('125oU8'))
-# len(s)==4 or 6 프린트하니까 그냥 len(s)값 나옴ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
-
 def solution(s):
     if len(s)==4 or len(s)==6:
         for i in s:
