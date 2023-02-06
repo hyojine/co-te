9cef63e27533cd36344f41c59729aa693b9c089c
hyojine <touroz@naver.com>
2023-02-06 22:42:04 +0900

    1주차 5번 완료(2)

--- 1주차/5.숫자문자열과영단어.py
+++ 1주차/5.숫자문자열과영단어.py
@@ -5,3 +5,12 @@ def solution(s):
             for i in range(s.count(letter)):
                 s=s.replace(letter,str(letters.index(letter)))
     return int(s)
+
+def solution(s):
+    letters=['zero','one','two','three','four','five','six','seven','eight','nine']
+    for letter in letters:
+        if letter in s:
+            s=s.replace(letter,str(letters.index(letter)),s.count(letter))
+    return int(s)
+
+print(solution('threethreethree'))
