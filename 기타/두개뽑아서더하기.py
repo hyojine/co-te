f317e007c42854f87c68fed21374d296aa2de08c
hyojine <touroz@naver.com>
2023-02-11 01:07:46 +0900

    기타 완료(2)

--- 기타/두개뽑아서더하기.py
+++ 기타/두개뽑아서더하기.py
@@ -6,3 +6,8 @@ def solution(numbers):
     for a in all:
         ans.append(sum(a))
     return sorted(list(set(ans)))
+
+from itertools import combinations
+
+def solution(numbers):
+    return sorted(list(set([sum(x) for x in list(combinations(numbers,2))])))
