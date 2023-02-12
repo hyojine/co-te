e1bce6c0919e41749d5893f5f6a7b426c21d7e28
hyojine <touroz@naver.com>
2023-02-12 23:51:54 +0900

    기타 완료

---
+++ 기타/최대공약수와최소공배수.py
@@ -0,0 +1,11 @@
+def solution(n, m):
+    ans=[]
+    for i in range(min(n,m),0,-1):
+        if n%i==0 and m%i==0:
+            ans.append(i)
+            break
+    for i in range(max(n,m),n*m+1):
+        if i%n==0 and i%m==0:
+            ans.append(i)
+            return ans
+    
