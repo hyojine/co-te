2328e5a89cb68d279f54cec31a5d6cfc6c00f46b
hyojine <touroz@naver.com>
2023-02-15 00:12:06 +0900

    기타 완료(2)

--- 기타/2016년.py
+++ 기타/2016년.py
@@ -2,4 +2,11 @@ def solution(a, b):
     days=[31,29,31,30,31,30,31,31,30,31,30,31]
     week=['THU','FRI','SAT','SUN','MON','TUE','WED',]
     idx=(sum(days[:a-1])+b)%7
+    print(days[:a],week[idx])
     return week[idx]
+
+import datetime
+
+def solution(a, b):
+    days=['MON','TUE','WED','THU','FRI','SAT','SUN']
+    return days[datetime.date(2016,a,b).weekday()]
