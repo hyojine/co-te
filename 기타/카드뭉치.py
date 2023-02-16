70205f916c7e1fb61442e4f72dcd5bc733b73476
hyojine <touroz@naver.com>
2023-02-17 00:19:25 +0900

    기타 완료

--- 기타/카드뭉치.py
+++ 기타/카드뭉치.py
@@ -1,5 +1,4 @@
 # goal에서의 순서랑 뭉치에서의 순서가 같으면 됨
-
 def solution(cards1, cards2, goal):
     idx_list=[]
     for card in cards1:
@@ -7,24 +6,42 @@ def solution(cards1, cards2, goal):
             idx_list.append(goal.index(card))
             goal.remove(card)
     for i in range(len(idx_list)-1):
-        if idx_list[i]>idx_list[i+1]:
+        if idx_list[i]>idx_list[i+1]: # 인덱스가 2이상 차이나도 됨
             return "No"
     idx_list.clear()
     for card in cards2:
         if card in goal:
             idx_list.append(goal.index(card))
     for i in range(len(idx_list)-1):
-        if idx_list[i]>idx_list[i+1]:
+        if idx_list[i]>idx_list[i+1]: # 인덱스가 2이상 차이나도 됨
             return "No"
     return "Yes"
 
 def solution(cards1, cards2, goal):
     i,j=0,0
     for card in goal:
-        if card in cards1[i:]:
+        if card in cards1[i:]: # 인덱스가 2이상 차이나도 됨
             i=cards1.index(card)+1
-        elif card in cards2[j:]:
+        elif card in cards2[j:]: # 인덱스가 2이상 차이나도 됨
             j=cards2.index(card)+1
+        else:
+            return "No"
+    return "Yes"
+
+# 카드를 사용하지 않고 다음 카드로 넘어갈 수 없습니다. == 무조건 인덱스가 1씩만 차이나야함
+def solution(cards1, cards2, goal):
+    i,j=0,0
+    for card in goal:
+        if card in cards1:
+            if cards1.index(card)==i:
+                i+=1
+            else:
+                return "No"
+        elif card in cards2:
+            if cards2.index(card)==j:
+                j+=1
+            else:
+                return "No"    
         else:
             return "No"
     return "Yes"
