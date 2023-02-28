628c65174a5b2034c8c29bf29d10620524591305
hyojine <touroz@naver.com>
2023-03-01 00:50:37 +0900

    기타 완료(2)

--- 기타/명예의전당(1).py
+++ 기타/명예의전당(1).py
@@ -1,12 +1,11 @@
 def solution(k, score):
     ans=[]
-    if len(score)>=k:
-        for i in range(len(score)):
-            if i>=k:
-                ans.append(sorted(score[:i+1],reverse=True)[k-1])
-            else:
-                ans.append(sorted(score[:i+1],reverse=True)[i])
-    else:
-        for i in range(len(score)):
+    for i in range(len(score)):
+        if i>=k:
+            ans.append(sorted(score[:i+1],reverse=True)[k-1])
+        else:
             ans.append(sorted(score[:i+1],reverse=True)[i])
     return ans
+
+def solution(k, score):
+    return [sorted(score[:i+1],reverse=True)[k-1] if i>=k else sorted(score[:i+1],reverse=True)[i] for i in range(len(score)) ]
