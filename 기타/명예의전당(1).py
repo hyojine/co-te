c753aa3e29389d0b16d040fb9650f73628741fea
hyojine <touroz@naver.com>
2023-03-01 00:51:42 +0900

    기타 완료(3)

--- 기타/명예의전당(1).py
+++ 기타/명예의전당(1).py
@@ -9,3 +9,16 @@ def solution(k, score):
 
 def solution(k, score):
     return [sorted(score[:i+1],reverse=True)[k-1] if i>=k else sorted(score[:i+1],reverse=True)[i] for i in range(len(score)) ]
+
+# ans의 마지막 값과 비교하는 조건문 추가
+def solution(k, score):
+    ans=[]
+    for i in range(len(score)):
+        if i>=k:
+            if ans[-1]<score[i]:
+                ans.append(sorted(score[:i+1],reverse=True)[k-1])
+            else:
+                ans.append(ans[-1])
+        else:
+            ans.append(sorted(score[:i+1],reverse=True)[i])
+    return ans
