104ae3a7cae588f22fc80993ae4c9168300e2dd7
hyojine <touroz@naver.com>
2023-03-01 23:45:36 +0900

    기타 완료

---
+++ 기타/완주하지못한선수.py
@@ -0,0 +1,7 @@
+def solution(participant, completion):
+    if len(participant)==len(set(participant)):
+        return (set(participant)-set(completion)).pop()
+    else:
+        for i,j in zip(sorted(participant),sorted(completion)):
+            if i != j:
+                return i
