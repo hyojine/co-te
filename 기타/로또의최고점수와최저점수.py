92dcfaff390a1bbbce94ef7a775e54123f9a9d36
hyojine <touroz@naver.com>
2023-03-03 17:21:33 +0900

    기타 완료(3)

--- 기타/로또의최고점수와최저점수.py
+++ 기타/로또의최고점수와최저점수.py
@@ -1,12 +1,8 @@
 def solution(lottos, win_nums):
-    pos_nums=set(win_nums)-set(lottos)
     inter_nums=set(win_nums)&set(lottos)
     zero_cnt=lottos.count(0)
     lowest=len(inter_nums)
-    if zero_cnt>=len(pos_nums):
-        highest=lowest+len(pos_nums)
-    else:
-        highest=lowest+zero_cnt
+    highest=lowest+zero_cnt
     if highest==0:
         lowest,highest=1,1
     elif lowest==0:
