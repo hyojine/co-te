5f508af8fc2d136b9b3f6e9fdae33190e67b1038
hyojine <touroz@naver.com>
2023-03-07 15:18:53 +0900

    기타 완료(2)

--- 기타/옹알이(2).py
+++ 기타/옹알이(2).py
@@ -2,9 +2,26 @@ def solution(babbling):
     answer = 0
     for i in babbling:
         for j in ['aya','ye','woo','ma']:
-            if j*2 not in i:
+            if j*2 not in i:# 연속해서 같은 발음을 할 수는 없음
+                i=i.replace(j,'')
+                print('i=',i)
+        if len(i)==0:
+            answer +=1
+    return answer
+
+# break하는 게 더 느림
+def solution_break(babbling):
+    answer = 0
+    for i in babbling:
+        for j in ['aya','ye','woo','ma']:
+            if j*2 in i:
+                break
+            else:
                 i=i.replace(j,' ')
-                print("i =", i)
         if len(i.strip())==0:
             answer +=1
     return answer
+
+
+
+# print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))
