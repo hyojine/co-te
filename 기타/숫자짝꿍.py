85de369dfc4ee4c8ad82dd4295defa243c6fb226
hyojine <touroz@naver.com>
2023-03-11 01:19:31 +0900

    기타 완료

--- 기타/숫자짝꿍.py
+++ 기타/숫자짝꿍.py
@@ -1,35 +1,40 @@
-# a=123
-# print([a])
-# print(str(a))
-# print(list(a))# 'int' object is not iterable
+# https://school.programmers.co.kr/learn/courses/30/lessons/131128
 
-# 계속 %10해서 한자리수를 뺄 수는 있쥬?
-
-# print(int('0000')) #0
-num_dict={'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-for i in num_dict.keys():
-    print(i)
-
-# 시간초과
 def solution(X, Y):
-    num_dict={'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    for fac in str(X):
-        for num in num_dict.keys():
-            if num==fac:
-                num_dict[num]+=1
-    num_dict2={'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-    for fac in str(Y):
-        for num in num_dict2.keys():
-            if num==fac:
-                num_dict2[num]+=1
-    ans=[]
-    for num in list(num_dict.keys()):
-        if num_dict[num]==0 or num_dict2[num]==0:
-            continue
-        else:
-            ans.append(num*min(num_dict[num],num_dict2[num]))
-    if ans==[]:
+    X,Y=str(X),str(Y)
+    xandy=list(set(X)&set(Y))
+    if not xandy:
         return '-1'
-    if int(''.join(reversed(ans))) == 0:
+    elif xandy == ['0']:
         return '0'
-    return ''.join(reversed(ans))
+    xandy.sort(reverse=True)
+    xydic={}
+    answer=[]
+    for xy in xandy:
+        xydic[xy]=min(X.count(xy),Y.count(xy))
+    for k,v in xydic.items():
+        answer.append(k*v)
+    return ''.join(answer)
+
+# 테스트 11 〉	통과 (166.20ms, 29.9MB)
+# 테스트 12 〉	통과 (178.49ms, 30.1MB)
+# 테스트 13 〉	통과 (206.19ms, 30.1MB)
+# 테스트 14 〉	통과 (166.98ms, 29.9MB)
+# 테스트 15 〉	통과 (140.43ms, 29.8MB)
+
+def solution(X, Y):
+    X,Y=str(X),str(Y)
+    xandy=list(set(X)&set(Y))
+    if not xandy:
+        return '-1'
+    elif xandy == ['0']:
+        return '0'
+    xandy.sort(reverse=True)
+    answer=[xy*min(X.count(xy),Y.count(xy)) for xy in xandy ]
+    return ''.join(answer)
+
+# 테스트 11 〉	통과 (139.16ms, 30MB)
+# 테스트 12 〉	통과 (131.48ms, 29.9MB)
+# 테스트 13 〉	통과 (123.17ms, 30MB)
+# 테스트 14 〉	통과 (134.14ms, 30MB)
+# 테스트 15 〉	통과 (130.26ms, 30.1MB)
