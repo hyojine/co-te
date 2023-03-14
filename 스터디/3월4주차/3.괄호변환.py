7ccb10edfc04621548fe085d47d39960d56d5fb2
hyojine <touroz@naver.com>
2023-03-14 23:54:24 +0900

    스터디 3번 미완료

---
+++ 스터디/3월4주차/3.괄호변환.py
@@ -0,0 +1,40 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/60058 
+# 균형잡힌 괄호 문자열: '(' 의 개수와 ')' 의 개수가 같다
+# 올바른 괄호 문자열: '('와 ')'의 괄호의 짝도 모두 맞을 경우
+
+def check(u):
+    stack=[]
+    for uu in u:
+        if uu=='(':
+            stack.append(uu)
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    return True
+
+def solution(p):
+    if not p:
+        return ''
+    p_open,p_close=0,0
+    for i in range(len(p)):
+        if p[i]=='(':
+            p_open+=1
+        else:
+            p_close+=1
+        if p_open == p_close:
+            u,v= p[:i+1],p[i+1:]
+    
+    if check(u):
+        return u+solution(v)
+    else:
+        answer='('
+        answer+=solution(v)
+        answer+=')'
+        
+        for uu in u[1:len(u) - 1]:
+            if uu == '(':
+                answer += ')'
+            else:
+                answer += '('
+        return answer
