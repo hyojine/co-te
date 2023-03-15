d59b9a6a59cecf74736fc4d7ae8ad15d5c91e4af
hyojine <touroz@naver.com>
2023-03-15 18:01:14 +0900

    스터디 6번 완료(2)

--- 스터디/3월4주차/6.설탕배달.py
+++ 스터디/3월4주차/6.설탕배달.py
@@ -12,6 +12,20 @@ def solution(n):
         if n in (4,7):
             return -1
         return n//5+2
-        
+
 N=int(input())
 print(solution(N))
+
+#2
+def solution(n):
+    cnt = 0
+    while n >= 0:
+        if n % 5 == 0:
+            cnt += n // 5
+            return cnt
+        n -= 3
+        cnt += 1
+    return -1
+
+n=int(input())
+print(solution(n))
