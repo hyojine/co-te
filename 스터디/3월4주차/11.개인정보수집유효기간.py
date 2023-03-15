4ef921350687d29761bf4cc896ecf53d6dc37ff8
hyojine <touroz@naver.com>
2023-03-15 22:53:15 +0900

    스터디 11번 완료

---
+++ 스터디/3월4주차/11.개인정보수집유효기간.py
@@ -0,0 +1,36 @@
+# 개인정보 수집일자에 약관종류에 따른 기간을 더한 후 오늘 날짜랑 비교
+# 오늘 날짜랑 년 같으면-> 월 -> 일 순으로 비교
+# privacis의 날짜는 today 이전의 날짜만 주어집니다.
+def solution(today, terms, privacies):
+    terms=dict([x.split(' ') for x in terms])# 약관종류,유효기간
+    # print(terms) # {'A': '6', 'B': '12', 'C': '3'}
+    privacies=[x.split(' ') for x in privacies]# 날짜,약관종류
+    # print(privacies) # [['2021.05.02', 'A'], ['2021.07.01', 'B'], ['2022.02.19', 'C'], ['2022.02.20', 'C']]
+    today=[int(x) for x in today.split('.')]# y,m,d
+    # print(today) #[2022, 5, 19]
+    answer = []
+    for i,p in enumerate(privacies):
+        date, kinds = p
+        y,m,d=map(int,date.split('.'))
+        d=d+int(terms[kinds])*28
+        if d>28:
+            if d%28 != 0:
+                m+=d//28
+                d=d%28
+            else:
+                m+=d//28-1
+                d=28
+        if m>12:
+            if m%12 != 0:
+                y+=m//12
+                m=m%12
+            else:
+                y+=m//12-1
+                m=12
+        if y<today[0]:
+            answer.append(i+1)
+        elif y==today[0] and m<today[1]:
+            answer.append(i+1)
+        elif y==today[0] and m==today[1] and d<=today[2]:
+            answer.append(i+1)
+    return answer
