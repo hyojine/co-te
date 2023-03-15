cfb4ed93f81a0ba776738e6e06e0b05e3dd5ef41
hyojine <touroz@naver.com>
2023-03-15 09:29:22 +0900

    스터디 6번 완료

---
+++ 스터디/3월4주차/6.설탕배달.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/2839
+# N킬로그램을 배달시 배달해야하는 봉지의 최소 개수
+# 3킬로그램 봉지와 5킬로그램 봉지
+# N은 3kg 이상 5000kg 이하
+
+def solution(n):
+    if n%5 ==0:
+        return n//5
+    elif n%5 in (1,3):
+        return n//5+1
+    elif n%5 in (2,4):
+        if n in (4,7):
+            return -1
+        return n//5+2
+        
+N=int(input())
+print(solution(N))
