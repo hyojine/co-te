9178bfdb4363604d713d74e7e2bc0f57ccb5845b
hyojine <touroz@naver.com>
2023-03-17 00:22:57 +0900

    기타 완료

--- 기타/신고결과받기.py
+++ 기타/신고결과받기.py
@@ -1,5 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92334
 # 유저별로 처리 결과 메일을 받은 횟수
+
+# 95.8
 def solution(id_list, report, k):
     re_result = []
     ans=[]
@@ -21,3 +23,18 @@ def solution(id_list, report, k):
         else:
             ans.append(0) 
     return ans
+
+# 통과
+from collections import Counter
+
+def solution(id_list, report, k):
+    
+    ans=dict([x for x in zip(id_list,[0]*len(id_list))]) 
+    er_ee=[x.split(' ') for x in set(report)]
+    re_list= [kk for kk,v in Counter([x[1] for x in er_ee]).items() if v>=k]
+        
+    for e in er_ee:
+        er,ee=e
+        if ee in re_list:
+            ans[er]+=1
+    return list(ans.values())
