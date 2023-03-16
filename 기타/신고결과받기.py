c20c1f3a77c6df6897ae435d1f7f5908099c94b4
hyojine <touroz@naver.com>
2023-03-16 23:53:26 +0900

    기타 미완료

---
+++ 기타/신고결과받기.py
@@ -0,0 +1,23 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/92334
+# 유저별로 처리 결과 메일을 받은 횟수
+def solution(id_list, report, k):
+    re_result = []
+    ans=[]
+    answer=[]
+    cnt=dict([x for x in zip(id_list,[0]*len(id_list))]) # 유저별 신고당한 횟수
+    er_ee=[x.split(' ') for x in set(report)] #(신고한 사람,신고 당한 사람)
+    # 유저별 신고 당한 수를 cnt에 저장
+    for e in er_ee:
+        er,ee=e
+        cnt[ee]+=1
+        if cnt[ee]>=k:
+            re_result.append(ee)
+    for e in er_ee:
+        if e[1] in re_result:
+            answer.append(e[0])
+    for id in id_list:
+        if id in answer:
+            ans.append(answer.count(id))
+        else:
+            ans.append(0) 
+    return ans
