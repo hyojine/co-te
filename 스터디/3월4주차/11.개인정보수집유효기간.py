55344e22914c321a02d96c11215653176c85cd0b
hyojine <touroz@naver.com>
2023-03-19 13:31:47 +0900

    스터디 11번 완료(2)

--- 스터디/3월4주차/11.개인정보수집유효기간.py
+++ 스터디/3월4주차/11.개인정보수집유효기간.py
@@ -1,7 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/150370
+
 # 개인정보 수집일자에 약관종류에 따른 기간을 더한 후 오늘 날짜랑 비교
 # 오늘 날짜랑 년 같으면-> 월 -> 일 순으로 비교
 # privacis의 날짜는 today 이전의 날짜만 주어집니다.
-def solution(today, terms, privacies):
+
+def solution0(today, terms, privacies):
     terms=dict([x.split(' ') for x in terms])# 약관종류,유효기간
     # print(terms) # {'A': '6', 'B': '12', 'C': '3'}
     privacies=[x.split(' ') for x in privacies]# 날짜,약관종류
@@ -32,5 +35,22 @@ def solution(today, terms, privacies):
         elif y==today[0] and m<today[1]:
             answer.append(i+1)
         elif y==today[0] and m==today[1] and d<=today[2]:
+            answer.append(i+1)
+    return answer
+
+# 총 일수 비교하는 방법
+def solution(today, terms, privacies):
+    today=list(map(int,today.split('.')))
+    std=today[0]*12*28+today[1]*28+today[2]
+    terms=dict([x.split(' ') for x in terms])
+    privacies=[x.split(' ') for x in privacies]
+    answer=[]
+    for i,p in enumerate(privacies):
+        date,kind = p
+        date=list(map(int,date.split('.')))
+        y=date[0]
+        m=date[1]
+        d=date[2]
+        if y*12*28+m*28+d+int(terms[kind])*28<=std:
             answer.append(i+1)
     return answer
