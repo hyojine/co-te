dfc09877c80641dbb3adbcf0b8ab0a59f307661e
hyojine <touroz@naver.com>
2023-03-26 00:49:53 +0900

    스터디 8번 완료(3)

--- 스터디/3월5주차/8.수찾기.py
+++ 스터디/3월5주차/8.수찾기.py
@@ -36,3 +36,53 @@ for mm in mlist:
             start=(start+end)//2+1
     if start>end:
         print(0)
+
+# 이분탐색 함수형
+import sys
+
+def solution(target,start,end):
+    while start<=end:
+        mid=(start+end)//2
+        if target==nlist[mid]:
+            return 1
+        elif target < nlist[mid]:
+            end=mid-1
+        elif target > nlist[mid]:
+            start=mid+1
+    if start>end:
+        return 0
+
+n=int(input())
+nlist=sorted(map(int,sys.stdin.readline().split()))
+m=int(input())
+mlist=list(map(int,sys.stdin.readline().split()))
+
+for mm in mlist:
+    start=0
+    end=n-1
+    print(solution(mm,start,end))
+
+# 재귀
+import sys
+
+def solution(target,start,end):
+    if start>end:
+        return 0
+    mid=(start+end)//2
+    if target==nlist[mid]:
+        return 1
+    elif target < nlist[mid]:
+        end=mid-1
+    elif target > nlist[mid]:
+        start=mid+1
+    return solution(target,start,end)
+
+n=int(input())
+nlist=sorted(map(int,sys.stdin.readline().split()))
+m=int(input())
+mlist=list(map(int,sys.stdin.readline().split()))
+
+for mm in mlist:
+    start=0
+    end=n-1
+    print(solution(mm,start,end))
