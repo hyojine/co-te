146c2acfb32a214de789aebd32768bd297ed8a8a
hyojine <touroz@naver.com>
2023-04-04 02:39:20 +0900

    스터디 2번 완료(2)

--- 스터디/4월1주차/2.9.PalindromeNumber.py
+++ 스터디/4월1주차/2.9.PalindromeNumber.py
@@ -5,3 +5,15 @@ class Solution:
         x=str(x)
         if x==x[::-1]:
             return True
+
+# Follow up: Solve it without converting the integer to a string.     
+class Solution:
+    def isPalindrome(self, x: int) -> bool:        
+        if x<0:
+            return False
+        ans=0
+        org=x
+        while x:
+            ans=ans*10+x%10
+            x=x//10
+        return org==ans
