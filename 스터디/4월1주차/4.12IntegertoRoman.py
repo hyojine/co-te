c5e6507ea8cb5058c690c9fbf14f6d9f66f1c35e
hyojine <touroz@naver.com>
2023-04-04 10:22:35 +0900

    스터디 4번 완료

---
+++ 스터디/4월1주차/4.12IntegertoRoman.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        ans=''
+        roman={1000:'M',500:'D',100:'C',50:'L',10:'X',5:'V',1:'I',}
+        for i,k in enumerate(roman.keys()):
+            if num//k !=4:
+                ans+=(num//k)*roman[k]
+            else:
+                if ans:
+                    if ans[-1]==roman[list(roman.keys())[i-1]]:
+                        ans=ans[:-1]
+                        ans+=roman[k]+(roman[list(roman.keys())[i-2]])
+                    else:
+                        ans+=roman[k]+(roman[list(roman.keys())[i-1]])
+                else:
+                    ans+=roman[k]+(roman[list(roman.keys())[i-1]])
+            num=num%k
+        return ans
