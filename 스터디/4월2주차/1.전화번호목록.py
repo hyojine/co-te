0d0ff9a07c97ac03804cd997e8bd53ca1944134e
hyojine <touroz@naver.com>
2023-04-11 23:51:43 +0900

    1번 미완료

---
+++ 스터디/4월2주차/1.전화번호목록.py
@@ -0,0 +1,28 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+def solution(phone_book):
+    answer = True
+    for p in phone_book:
+        for q in phone_book:
+            if len(p)<len(q):
+                if q[:len(p)]==p:
+                    answer=False
+    return answer
+
+# 테스트 14 〉	통과 (154.84ms, 10.1MB)
+# 테스트 15 〉	통과 (322.18ms, 10.2MB)
+# 테스트 16 〉	통과 (268.53ms, 10.5MB)
+# 테스트 17 〉	통과 (388.18ms, 10.3MB)
+# 테스트 18 〉	통과 (867.69ms, 10.2MB)
+# 테스트 19 〉	통과 (1009.32ms, 10.3MB)
+# 테스트 20 〉	통과 (1726.15ms, 10.3MB)
+
+# if len(p)<len(q) and q[:len(p)]==p: # 조건문 두개 and로 변경시
+
+# 테스트 14 〉	통과 (142.89ms, 10.2MB)
+# 테스트 15 〉	통과 (273.42ms, 10.4MB)
+# 테스트 16 〉	통과 (243.70ms, 10.2MB)
+# 테스트 17 〉	통과 (383.78ms, 10.2MB)
+# 테스트 18 〉	통과 (695.01ms, 10.4MB)
+# 테스트 19 〉	통과 (1272.28ms, 10.4MB)
+# 테스트 20 〉	통과 (1337.49ms, 10.2MB)
