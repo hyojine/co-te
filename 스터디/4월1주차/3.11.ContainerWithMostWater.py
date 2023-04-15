fe394d7a8b497a0eb12b86a06554732253a515c6
hyojine <touroz@naver.com>
2023-04-15 23:59:12 +0900

    스터디 3번 완료

--- 스터디/4월1주차/3.11.ContainerWithMostWater.py
+++ 스터디/4월1주차/3.11.ContainerWithMostWater.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/container-with-most-water/description/
 
+# 이중 for문으로 TimeLimitExceeded
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         water=0
@@ -10,3 +11,17 @@ class Solution:
                 if j*min(height[i],height[i+j])>water:
                     water=j*min(height[i],height[i+j])
         return water
+       
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        start=0
+        end=len(height)-1
+        answer=0
+        while start<=end:
+            area=min(height[end],height[start])*(end-start)
+            answer=max(answer,area)
+            if height[end]>height[start]:
+                start+=1
+            else:
+                end-=1
+        return answer
