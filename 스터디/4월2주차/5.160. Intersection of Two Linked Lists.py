745eba8e8a48e1f9f1a1d3cd331c4dd71be543f7
hyojine <touroz@naver.com>
2023-04-20 22:26:01 +0900

    5번 완료

---
+++ 스터디/4월2주차/5.160. Intersection of Two Linked Lists.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a, b= headA, headB
+        while a!=b:
+          if a:
+            a=a.next
+          else:
+            a=headB 
+          if b:
+            b=b.next
+          else:
+            b=headA
+        return a
