b2de8fe3d328bc6129ae7a1239cf1ddd3fcc3437
hyojine <touroz@naver.com>
2023-04-22 23:47:25 +0900

    4번 완료

--- 스터디/4월2주차/4.달리기경주.py
+++ 스터디/4월2주차/4.달리기경주.py
@@ -7,6 +7,20 @@ def solution(players, callings):
         players[p-1],players[p]=players[p],players[p-1]
     return players
 
+# 딕셔너리 1개
+def solution(players, callings):
+    player_indexes = {player: index for index, player in enumerate(players)}
+
+    for j in callings:
+        current_index = player_indexes[j]
+        desired_index = current_index - 1
+        if current_index > 0 and players[desired_index] != j:
+            players[current_index], players[desired_index] = players[desired_index], players[current_index]
+            player_indexes[players[current_index]] = current_index
+            player_indexes[players[desired_index]] = desired_index
+    return players
+
+# 딕셔너리 2개 - 제일 빠름
 def solution(players, callings):
     dic_players = {p:i for i,p in enumerate(players)}
     dic_rank = {i:p for i,p in enumerate(players)}
@@ -14,7 +28,6 @@ def solution(players, callings):
     for call in callings :
         call_rank = dic_players[call] 
         call_front = dic_rank[call_rank - 1] 
-        
         
         dic_players[call], dic_players[call_front] = dic_players[call_front], dic_players[call]
         dic_rank[call_rank], dic_rank[call_rank - 1] = dic_rank[call_rank - 1], dic_rank[call_rank]
