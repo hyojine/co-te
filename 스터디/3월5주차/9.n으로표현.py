2e52fbac6feff311979e5356b5957b35fae4e440
hyojine <touroz@naver.com>
2023-04-24 23:58:58 +0900

    스터디 9번 미완료

--- 스터디/3월5주차/9.n으로표현.py
+++ 스터디/3월5주차/9.n으로표현.py
@@ -2,13 +2,13 @@
 
 def solution(N, number):
     answer = -1
-    DP = []
+    DP = [[] for _ in range(9)]
     for i in range(1, 9):
         numbers = set()
         numbers.add( int(str(N) * i) )
-        for j in range(0, i//2):
+        for j in range(1, (i+1)//2):
             for x in DP[j]:
-                for y in DP[-j-1]:
+                for y in DP[i-j]:
                     numbers.add(x + y)
                     numbers.add(x - y)
                     numbers.add(y - x)
@@ -20,6 +20,7 @@ def solution(N, number):
         if number in numbers:
             answer = i
             break
-        DP.append(numbers)
-
+        DP[i]=numbers
+        # DP.append(numbers)
+    print(DP)
     return answer
