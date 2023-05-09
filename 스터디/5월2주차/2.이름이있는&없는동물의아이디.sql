f9f69651292d677795bf554495ecd4227a51c546
hyojine <touroz@naver.com>
2023-05-09 23:25:38 +0900

    2번 완료

---
+++ 스터디/5월2주차/2.이름이있는&없는동물의아이디.sql
@@ -0,0 +1,14 @@
+-- 이름이 있는 동물의 아이디 & 이름이 없는 동물의 아이디
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59407
+
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59039
+
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID
