6a0f377fa97c2011e7bb760f2c502c7f1e55a59c
hyojine <touroz@naver.com>
2023-05-26 22:54:47 +0900

    4번 완료

---
+++ 스터디/5월4주차/4.루시와 엘라 찾기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59046
+
+SELECT animal_id, name, sex_upon_intake
+from animal_ins
+where name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+order by animal_id
