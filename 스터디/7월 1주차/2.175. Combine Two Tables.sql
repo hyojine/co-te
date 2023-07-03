21a07bdbf101c5950d97d3eb739b32677816fddd
Hyojin <112310580+hyojine@users.noreply.github.com>
2023-07-04 00:42:24 +0900

    2번 완료

---
+++ 스터디/7월 1주차/2.175. Combine Two Tables.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/combine-two-tables/description/
+
+select p.firstname, p.lastname, a.city, a.state
+from person p
+left outer join address a
+on p.personid=a.personid
