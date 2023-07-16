4352c0204bc0ae342ebfe3b23a2259cfb42abbd3
Hyojin <112310580+hyojine@users.noreply.github.com>
2023-07-17 00:35:19 +0900

    2번 완료

---
+++ 스터디/7월2주차/2.610. Triangle Judgement.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/triangle-judgement/submissions/
+
+select  *,(case when x<y+z and y<z+x and z<x+y then 'Yes'
+                else 'No'
+                end ) as triangle
+from triangle
