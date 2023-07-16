1079ec728a60b15e21c433509233cb0412a0caea
Hyojin <112310580+hyojine@users.noreply.github.com>
2023-07-17 01:03:15 +0900

    1번 완료(2)

--- 스터디/7월2주차/1.619. Biggest Single Number.sql
+++ 스터디/7월2주차/1.619. Biggest Single Number.sql
@@ -1,5 +1,6 @@
 -- https://leetcode.com/problems/biggest-single-number/submissions/
 
+-- not exists & union 사용 -> not exists이하의 쿼리의 값이 없을 때 의도한 값을 출력함
 (select num
  from mynumbers
  group by num
@@ -15,3 +16,19 @@ union
                    having count(num)=1
                    order by num desc
                    limit 1))
+
+-- 집계함수 사용 -> 서브쿼리에서 하나만 뽑았으므로 avg, min 등 모두 가능
+select max(num) num
+from (select num
+      from mynumbers
+      group by num
+      having count(num)=1
+      order by num desc
+      limit 1) a
+
+-- 집계함수 사용(2) -> 어차피 밖에서 max를 뽑을 거라면 order by, limit 필요 없음
+select max(num) num
+from (select num
+      from mynumbers
+      group by num
+      having count(num)=1) a
